f4d8a5ec001ca71595e8ba9f9082a013e58aa685
Donatas Kukta <donatas.kukta1@gmail.com>
2022-01-19 13:24:49 +0200

    Add initial Example WebClient  (React App).

---
+++ Source-Code/Example.WebClient/Startup.cs
@@ -0,0 +1,57 @@
+using System.IO;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+
+namespace Example.WebClient
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/build";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            var webFileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "ClientApp\\build"));
+            
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = webFileProvider,
+                RequestPath = "/WebClient"
+            });
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = webFileProvider,
+                RequestPath = "/MicroAC.ServiceFabric/Example.WebClient/WebClient"
+            });
+        }
+    }
+}
