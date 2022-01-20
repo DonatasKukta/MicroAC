7e97ae794df1d0c9d31f307c52476b88f5566825
Donatas Kukta <donatas.kukta1@gmail.com>
2022-01-20 11:41:56 +0200

    Add Allow-All CORS policy in request manager.

--- Source-Code/MicroAC.RequestManager/Startup.cs
+++ Source-Code/MicroAC.RequestManager/Startup.cs
@@ -21,15 +21,29 @@ namespace MicroAC.RequestManager
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+
             services.AddRouting();
+
             services.AddControllers();
+
             services.AddSingleton<HttpClient>(new HttpClient());
+
             services.AddSingleton<IConfiguration>(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //TODO: Apply CORS policies
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true)
+                .WithExposedHeaders(
+                    _config.GetValue<string>("Timestamp:Header"), 
+                    "X-ServiceFabricRequestId"));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
