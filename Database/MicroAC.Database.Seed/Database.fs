4f0217eed899fa770a1cd12b1ad58a22fd96a9c3
Donatas Kukta <donatas.kukta1@gmail.com>
2022-02-13 01:27:13 +0200

    Seed database with generated data.

--- Database/MicroAC.Database.Seed/Database.fs
+++ Database/MicroAC.Database.Seed/Database.fs
@@ -1,6 +1,5 @@
 ﻿module Database
 
-open MicroAC.Persistence.DbDTOs
 open Microsoft.Data.SqlClient
 open Microsoft.EntityFrameworkCore
 open Microsoft.SqlServer.Management.Common
@@ -9,6 +8,10 @@ open Microsoft.SqlServer.Management.Smo
 open System.Collections
 open System.IO
 
+open MicroAC.Persistence.DbDTOs
+
+exception SeedingError of string
+
 // DBContext is initialised once
 let private  context = 
     let dbOptions = (new DbContextOptionsBuilder<MicroACContext>())
@@ -16,15 +19,47 @@ let private  context =
                         .Options
     new MicroACContext(dbOptions)
 
-let getFirstUser() = 
-    printfn "getFirstUser"
-    context.Users |> Seq.head
+let private seedValue = Generate.dataSeed
 
-let getLastUser() = 
-    printfn "getLastUser"
-    context.Users |> Seq.last
+let private containsSeed<'T> (entity:'T) : bool = 
+    match box entity with 
+        | :? User            as entity -> entity.Name.Contains seedValue
+        | :? RolesPermission as entity -> entity.Role.Contains seedValue
+        | :? Service         as entity -> entity.Name.Contains seedValue
+        | :? UsersRole       as entity -> entity.Role.Contains seedValue
+        | :? Organisation    as entity -> entity.Name.Contains seedValue
+        | :? Role            as entity -> entity.Name.Contains seedValue
+        | :? Permission      as entity -> entity.Value.Contains seedValue
+        | _ -> raise (SeedingError($"Entity type {entity.GetType().Name} not found during seed removal."))
+    
+let private removeSeed<'T when 'T: not struct> (set: DbSet<'T>)  = 
+    let seededEntities = Seq.filter containsSeed set 
+    set.RemoveRange(seededEntities)
+
+let removeSeededValues() = 
+    removeSeed context.Users 
+    removeSeed context.Permissions
+    removeSeed context.Roles
+    removeSeed context.RolesPermissions
+    removeSeed context.Services
+    removeSeed context.UsersRoles
+    removeSeed context.Organisations
+    printfn "Deleted %i rows that had '%s' value in them." (context.SaveChanges()) seedValue
+    
+let seed<'T> (data: seq<'T>) = 
+    match box data with 
+        | :? seq<User>            as s -> context.Users.AddRange(s) 
+        | :? seq<Permission>      as s -> context.Permissions.AddRange(s)
+        | :? seq<Role>            as s -> context.Roles.AddRange(s)
+        | :? seq<RolesPermission> as s -> context.RolesPermissions.AddRange(s)
+        | :? seq<Service>         as s -> context.Services.AddRange(s)
+        | :? seq<UsersRole>       as s -> context.UsersRoles.AddRange(s)
+        | :? seq<Organisation>    as s -> context.Organisations.AddRange(s)
+        | _ -> raise (SeedingError($"Data type {data.GetType().Name} not found during seeding."))
+
+    printfn "Created %i  rows of %A entity." (context.SaveChanges()) typeof<'T> 
    
-let backupScripts() = 
+let saveStateToScript(filename) = 
     let rec copy (enumerator: IEnumerator) (array: SqlSmoObject[]) (i:int) = 
         let onElement() = array.[i] <- enumerator.Current :?> SqlSmoObject
                           copy enumerator array (i+1)
@@ -39,7 +74,7 @@ let backupScripts() =
     let scripter = new Scripter(sqlServer)
     scripter.Options.ScriptSchema <- true
     scripter.Options.ScriptData <- true
-    scripter.Options.FileName <- Config.getPath "_backup.sql"
+    scripter.Options.FileName <- Config.getPath filename
     scripter.Options.ToFileOnly <- true
     scripter.Options.NoCommandTerminator <- true
     scripter.Options.WithDependencies <- true
@@ -48,4 +83,4 @@ let backupScripts() =
     let result = scripter.EnumScript recArray
 
     File.WriteAllLines(Config.getPath "_backup-output.txt", result)    
-    0
+    
--- Database/MicroAC.Database.Seed/Program.fs
+++ Database/MicroAC.Database.Seed/Program.fs
@@ -4,16 +4,32 @@ let printEmail (user: User) = printfn "User email: %A" (user.Email)
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello world!"
+    printfn "Hello world! This will generate data for performance tests!"
     printfn "Working folder is %s" (Config.getPath "")
 
-    Database.backupScripts()
+    Database.saveStateToScript("backup.sql")
     
-    let organisations =     Generate.organisations    20
-    let roles =             Generate.roles            10
+    let organisations =     Generate.organisations    40
+    let roles =             Generate.roles            20
     let services =          Generate.services         10
-    let permissions =       Generate.permissions      50 services
-    let rolesPermissions =  Generate.rolesPermissions 30 roles permissions
-    let users =             Generate.users            20 organisations
-    let usersRoles =        Generate.usersRoles       10 users roles
+    let permissions =       Generate.permissions      70   services
+    let rolesPermissions =  Generate.rolesPermissions 120  roles permissions
+    let users =             Generate.users            1000 organisations
+    let usersRoles =        Generate.usersRoles       10   users roles
+
+    // Clean database from previously seeded values.
+    Database.removeSeededValues()
+    Database.saveStateToScript("after_removing_seeded_data.sql")
+
+    Database.seed organisations    
+    Database.seed roles            
+    Database.seed services         
+    Database.seed permissions      
+    Database.seed rolesPermissions
+    Database.seed users          
+    Database.seed usersRoles     
+
+    Database.saveStateToScript("after_seeding_data.sql")
+    
+    printfn "Seeding completed succesfully."
     0
