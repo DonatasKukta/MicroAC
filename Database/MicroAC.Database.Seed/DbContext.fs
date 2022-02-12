59f468bd4dcfcb7d91ff9f7f885d0bb1f314da7e
Donatas Kukta <donatas.kukta1@gmail.com>
2022-02-12 13:07:56 +0200

    Add database seeding project with working DbContext (f#).

---
+++ Database/MicroAC.Database.Seed/DbContext.fs
@@ -0,0 +1,15 @@
+﻿module DbContext
+open MicroAC.Persistence.DbDTOs
+open FSharp.Data
+open Microsoft.EntityFrameworkCore
+
+[<Literal>]
+let private settingsFile = "appsettings.persistance.json"
+type private config = JsonProvider<settingsFile>
+
+let get() = 
+    let dbConnection = config.Load(settingsFile).ConnectionStrings.MicroAcDatabase
+    let dbOptions = (new DbContextOptionsBuilder<MicroACContext>())
+                        .UseSqlServer(dbConnection)
+                        .Options
+    new MicroACContext(dbOptions)
---
+++ Database/MicroAC.Database.Seed/Program.fs
@@ -0,0 +1,7 @@
+﻿[<EntryPoint>]
+let main argv =
+    printfn "Hello world!"
+    let dbContext = DbContext.get()
+    let firstUser = (dbContext.Users |> Seq.head)
+    printfn "Email of first user is %A" firstUser.Email
+    0
