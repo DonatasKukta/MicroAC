4e9405a77051eb439dfd1ce6c7fd07d5babcac41
Donatas Kukta <donatas.kukta1@gmail.com>
2022-02-12 22:16:57 +0200

    Create DB data generation logic.

--- Database/MicroAC.Database.Seed/Program.fs
+++ Database/MicroAC.Database.Seed/Program.fs
@@ -7,9 +7,13 @@ let main argv =
     printfn "Hello world!"
     printfn "Working folder is %s" (Config.getPath "")
 
-    printEmail (Database.getFirstUser())
-    printEmail (Database.getLastUser())
-
-    Database.backupScripts() |> ignore
-    //Generate.users 20
+    Database.backupScripts()
+    
+    let organisations =     Generate.organisations    20
+    let roles =             Generate.roles            10
+    let services =          Generate.services         10
+    let permissions =       Generate.permissions      50 services
+    let rolesPermissions =  Generate.rolesPermissions 30 roles permissions
+    let users =             Generate.users            20 organisations
+    let usersRoles =        Generate.usersRoles       10 users roles
     0
