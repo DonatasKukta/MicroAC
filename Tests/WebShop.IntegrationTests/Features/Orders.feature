d95fd87b65143d20b217c28ed3be8f8b08da3397
Donatas Kukta <donatas.kukta1@gmail.com>
2022-03-14 20:10:06 +0200

    Add empty WebShop.Orders project.

---
+++ Tests/WebShop.IntegrationTests/Features/Orders.feature
@@ -0,0 +1,6 @@
+﻿Feature: Orders
+
+Scenario: Get
+	Given GET request
+	When request is sent
+	Then response status code is 200
---
+++ Tests/WebShop.IntegrationTests/Steps/OrdersSteps.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using TechTalk.SpecFlow;
+
+namespace WebShop.IntegrationTests.Steps
+{
+    [Binding]
+    [Scope(Feature = "Orders")]
+    public sealed class OrdersSteps : SharedSteps
+    {
+        public OrdersSteps()
+            : base(new Uri("http://localhost:19083/MicroAC.ServiceFabric/WebShop.Orders/"))
+        {
+
+        }
+    }
+}
