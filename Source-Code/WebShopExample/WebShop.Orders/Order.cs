38cfaeda27a787f38afa5c85070af8321ee38db8
Donatas Kukta <donatas.kukta1@gmail.com>
2022-03-14 21:10:17 +0200

    Add Empty Orders controller and models.

---
+++ Source-Code/WebShopExample/WebShop.Orders/Order.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebShop.Orders
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public List<Item> Products { get; set; }
+        public PaymentDetails PaymentDetails { get; set; }
+        public ShipmentDetails ShipmentDetails { get; set; }
+    }
+
+    public class Item
+    {
+        public int ProductId { get; set; }
+        public DateTime AddedAt { get; set; }
+        public int Quantity { get; set; }
+        public decimal? Price { get; set; }
+        public decimal? Discount { get; set; }
+    }
+
+    public class PaymentDetails
+    {
+        public string CardNumber { get; set; }
+        public string SecurityCode { get; set; }
+        public string ExpiryDate { get; set; }
+        public decimal TotalSum { get; set; }
+    }
+
+    public class ShipmentDetails
+    {
+        public string Country { get; set; }
+        public string Municipality { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+        public string Cost { get; set; }
+        public string Provider { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ Source-Code/WebShopExample/WebShop.Orders/OrdersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebShop.Orders
+{
+    [Route("/")]
+    public class OrdersController : Controller
+    {
+        public OrdersController()
+        {
+
+        }
+
+        [HttpGet]
+        public Order GetOrder()
+        {
+            return new Order();
+        }
+    }
+}
