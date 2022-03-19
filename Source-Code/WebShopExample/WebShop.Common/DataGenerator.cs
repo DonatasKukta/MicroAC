b2a3db468d6e90da360b187a81787a951cf9f12b
Donatas Kukta <donatas.kukta1@gmail.com>
2022-03-19 21:14:19 +0200

    Add Order api endpoints and move data generation logic to Common project.

---
+++ Source-Code/WebShopExample/WebShop.Common/DataGenerator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using Bogus;
+
+namespace WebShop.Common
+{
+    public class DataGenerator
+    {
+        Faker<Order> FakeOrder;
+
+        Faker<Order.Item> FakeOrderItem;
+
+        Faker<Order.PaymentDetails> FakeOrderPaymentDetails;
+
+        Faker<Order.ShipmentDetails> FakeOrderShipmentDetails;
+
+        public DataGenerator()
+        {
+            FakeOrderItem = new Faker<Order.Item>()
+                .RuleFor(i => i.ProductId, f => f.Random.Int(1, 20))
+                .RuleFor(i => i.AddedAt, f => f.Date.Recent(0))
+                .RuleFor(i => i.Quantity, f => f.Random.Int(1, 5))
+                .RuleFor(i => i.Price, f => f.Random.Decimal(250, 1700))
+                .RuleFor(i => i.Discount, f => f.Random.Decimal(0.05m, 0.3m).OrNull(f));
+
+            FakeOrderPaymentDetails = new Faker<Order.PaymentDetails>()
+                .RuleFor(i => i.TotalSum, f => f.Finance.Amount())
+                .RuleFor(i => i.SecurityCode, "***")
+                .RuleFor(i => i.ExpiryDate, "**/**")
+                .RuleFor(i => i.CardNumber, f =>
+                 new string('*', 12) + f.Finance.CreditCardNumber().Substring(11));
+
+            FakeOrderShipmentDetails = new Faker<Order.ShipmentDetails>()
+                .RuleFor(i => i.Country, f => f.Address.Country())
+                .RuleFor(i => i.AddressLine1, f => f.Address.StreetAddress())
+                .RuleFor(i => i.AddressLine2, f => f.Address.SecondaryAddress())
+                .RuleFor(i => i.City, f => f.Address.City())
+                .RuleFor(i => i.PostCode, f => f.Address.ZipCode())
+                .RuleFor(i => i.Cost, f => f.Random.Decimal(4m, 30m))
+                .RuleFor(i => i.Provider, f => f.Company.CompanyName());
+
+            FakeOrder = new Faker<Order>()
+                .RuleForType(typeof(Guid), f => f.Random.Guid())
+                .RuleFor(i => i.Products, f => FakeOrderItem.GenerateBetween(1, 10))
+                .RuleFor(i => i.Payment, f => FakeOrderPaymentDetails.Generate())
+                .RuleFor(i => i.Shipment, f => FakeOrderShipmentDetails.Generate());
+        }
+
+        public Order GenerateOrder()
+        {
+            return FakeOrder.Generate();
+        }
+
+        public Order.PaymentDetails GenerateOrderPaymentDetails()
+        {
+            return FakeOrderPaymentDetails.Generate();
+        }
+
+        public Order.ShipmentDetails GenerateOrderShipmentDetails()
+        {
+            return FakeOrderShipmentDetails.Generate();
+        }
+    }
+}
--- Source-Code/WebShopExample/WebShop.Orders/OrdersController.cs
+++ Source-Code/WebShopExample/WebShop.Orders/OrdersController.cs
@@ -1,54 +1,74 @@
 ﻿using System;
 
-using Bogus;
 using Microsoft.AspNetCore.Mvc;
+
+using WebShop.Common;
 
 namespace WebShop.Orders
 {
     [Route("/")]
     public class OrdersController : Controller
     {
+        DataGenerator Data;
+
         public OrdersController()
         {
-
+            Data = new DataGenerator();
         }
 
-        [HttpGet]
-        public Order GetOrder()
+        [HttpGet("/{id}")]
+        public Order GetOrder([FromRoute] Guid id)
         {
-            return CreateFakeOrder();
+            var order = Data.GenerateOrder();
+            order.Id = id;
+            return order;
         }
 
-        Order CreateFakeOrder()
+        [HttpPost("/{cartId}")]
+        public ActionResult CreateOrder([FromRoute] Guid cartId)
         {
-            var fakeItem = new Faker<Item>()
-                .RuleFor(i => i.ProductId, f => f.Random.Int(1, 20))
-                .RuleFor(i => i.AddedAt, f => f.Date.Recent(0))
-                .RuleFor(i => i.Quantity, f => f.Random.Int(1,5))
-                .RuleFor(i => i.Price, f => f.Random.Decimal(250, 1700))
-                .RuleFor(i => i.Discount, f => f.Random.Decimal(0.05m, 0.3m).OrNull(f));
+            // Call Carts Api (Get)
+            // Call Carts Api (Delete)
+            // Call Products Api
 
-            var fakePayemnt = new Faker<PaymentDetails>()
-                .RuleFor(i => i.CardNumber, f => f.Finance.CreditCardNumber())
-                .RuleFor(i => i.SecurityCode, f => f.Finance.CreditCardCvv())
-                .RuleFor(i => i.ExpiryDate, f => f.Date.Past(3).ToString())
-                .RuleFor(i => i.TotalSum, f => f.Finance.Amount());
+            var order = Data.GenerateOrder();
+            
+            order.Shipment = null;
+            order.Payment = null;
 
-            var fakeShipmentDetails = new Faker<ShipmentDetails>()
-                .RuleFor(i => i.Country, f => f.Address.Country())
-                .RuleFor(i => i.AddressLine1, f => f.Address.StreetAddress())
-                .RuleFor(i => i.AddressLine2, f => f.Address.SecondaryAddress())
-                .RuleFor(i => i.City, f => f.Address.City())
-                .RuleFor(i => i.PostCode, f => f.Address.ZipCode())
-                .RuleFor(i => i.Cost, f => f.Random.Decimal(4m, 30m))
-                .RuleFor(i => i.Provider, f => f.Company.CompanyName());
+            return Created(Guid.NewGuid().ToString(), order);
+        }
 
-            return new Faker<Order>()
-                .RuleForType(typeof(Guid), f => f.Random.Guid())
-                .RuleFor(i => i.Products, f => fakeItem.GenerateBetween(1, 10))
-                .RuleFor(i => i.PaymentDetails, f => fakePayemnt.Generate())
-                .RuleFor(i => i.ShipmentDetails, f => fakeShipmentDetails.Generate())
-                .Generate();
+        [HttpDelete("/{orderId}")]
+        public ActionResult DeleteOrder([FromRoute] Guid orderId)
+        {
+            return Ok();
+        }
+
+        [HttpPut("/{orderId}")]
+        public ActionResult SubmitShipmentDetails(
+            [FromRoute] Guid orderId, 
+            [FromBody] Order.ShipmentDetails shipmentDetails)
+        {
+            // Call Shipment Api
+            return Created(Guid.NewGuid().ToString(), shipmentDetails);
+        }
+
+        [HttpPut("/{orderId}")]
+        public ActionResult SubmitPaymentDetails(
+            [FromRoute] Guid orderId,
+            [FromBody] Order.PaymentDetails paymentDetails)
+        {
+            return Created(Guid.NewGuid().ToString(), paymentDetails);
+        }
+
+        [HttpPut("/{orderId}")]
+        public Order SubmitOrder([FromRoute] Guid orderId)
+        {
+            // Call Shipment Api
+            // Call Payment Api
+            // Call Products Api
+            return GetOrder(orderId);
         }
     }
 }
